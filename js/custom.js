ef4543f57f106c86f41ffbd1f153d7ba294655d1
Mawiz Shahab <97441655+MawizShahab@users.noreply.github.com>
2023-10-31 20:24:28 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,43 @@ $(document).ready(function () {
   });
 });
 
+
+
+/* Hnadel active state */
+
+$(document).ready(function () {
+  // Retrieve the active category from local storage
+  var activeCategory = localStorage.getItem("activeCategory");
+
+  // Set the active class on the category link
+  if (activeCategory) {
+    $(".categories a[data-category='" + activeCategory + "']").addClass(
+      "active"
+    );
+  }
+
+  // Handle click on category links
+  $(".categories a").click(function (e) {
+    e.preventDefault(); // Prevent the default link behavior (page navigation)
+    var target = $(this).attr("href"); // Get the target URL
+    var category = $(this).data("category");
+
+    // Store the active category in local storage
+    localStorage.setItem("activeCategory", category);
+
+    // Set the active class on the category link
+    $(".categories a").removeClass("active"); // Remove active class from all links
+    $(this).addClass("active"); // Set active class on the clicked link
+
+    // Redirect to the target page after a short delay
+    setTimeout(function () {
+      window.location.href = target;
+    }, 100); // Adjust the delay as needed
+  });
+});
+
+/* jQuery code to toggle FAQ's */
+
 $(document).ready(function () {
   $(".question").click(function () {
     const answer = $(this).next(".answer");
@@ -31,19 +68,30 @@ $(document).ready(function () {
   });
 });
 
+/* To animate buttons */
+
 $(document).ready(function () {
   $(".text, .button-container").addClass("animate");
 });
 
+/* To toggle disover cards and footer-links for mobile only */
+
 $(document).ready(function () {
+  if ($(window).width() < 768) {
+    $(".links").hide();
+    $(".newsletter-subscription").insertBefore(".footer-end");
+  }
+
   $(".toggle-chevron").on("click", function () {
     $(this).toggleClass("up");
     $(this)
-      .closest(".discover-card")
-      .find(".card-description, .card-button")
+      .closest(".discover-card, .footer-links")
+      .find(".card-description, .card-button, .links, .label-seperator")
       .slideToggle();
   });
 });
+
+/* Animation code to animate the Discover Cards */
 
 $(document).ready(function () {
   $(window).on("scroll", function () {
@@ -72,13 +120,15 @@ $(document).ready(function () {
   });
 });
 
+/* Animation code to animate the other sections */
+
 $(document).ready(function () {
   $(window).on("scroll", function () {
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
     var scrollPos = $(window).scrollTop();
     var section = $(
-      ".filter-tabs-content, .tab-content.hide-by-default, .nav-tabs.hide-by-default"
+      ".filter-tabs-content, .tab-content.hide-by-default, .nav-tabs.hide-by-default, .speak-content, .button-container-speak-us, .faq-content, .faq-toggle"
     );
 
     // Define the minimum width for desktop
